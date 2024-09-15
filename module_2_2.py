f33f8196d62607033024beb316d15eaed7b93622
Лена <trylya@mail.ru>
2024-09-15 12:05:41 +0300

    условная конструкция

--- module_2_2.py
+++ module_2_2.py
@@ -1,8 +1,9 @@
-first=int(input('Введите число'))
-second=int(input('Введите число'))
-third=int(input('Введите число'))
-if first==second==third:
+first = int(input('Введите число'))
+second = int(input('Введите число'))
+third = int(input('Введите число'))
+if first == second == third:
     print(3)
-elif first==second or second==third or first==third:
+elif first == second or second == third or first == third:
     print(2)
-else: print(0)
+else:
+    print(0)
