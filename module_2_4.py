c4b97baf3a4babcb2eb511bb5127a02d82237406
Лена <trylya@mail.ru>
2024-09-20 21:03:10 +0300

    цикл for

--- module_2_4.py
+++ module_2_4.py
@@ -1,13 +1,11 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = []
-not_primes = []
-#is_prime = True
+primes = list()
+not_primes = list()
 for i in numbers:
     if i==1:
         continue
     for j in range (2,i//2+1):
         if i % j == 0:
-            #is_prime = False
             not_primes.append(i)
             break
     else:
